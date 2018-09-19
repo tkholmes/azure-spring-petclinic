ed4d1136045a05e0334d55c2051346fcb619b40f
Bruno Borges <bruno.borges@microsoft.com>
2018-09-19 07:23:40 -0700

    new feature

--- src/main/java/org/springframework/samples/petclinic/ServletInitializer.java
+++ src/main/java/org/springframework/samples/petclinic/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.microsoft.webdemo;
+package org.springframework.samples.petclinic;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(WebdemoApplication.class);
+		return application.sources(PetClinicApplication.class);
 	}
 
 }
--- src/main/java/org/springframework/samples/petclinic/system/ShowSystem.java
+++ src/main/java/org/springframework/samples/petclinic/system/ShowSystem.java
@@ -1,4 +1,32 @@
 package org.springframework.samples.petclinic.system;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+@RestController
 public class ShowSystem {
+
+    @GetMapping(value = "/showsystem", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Properties showSystem() {
+        /*
+        final Map<String, String> map = new HashMap<>();
+        Properties p = System.getProperties();
+        Enumeration keys = p.keys();
+        while (keys.hasMoreElements()) {
+            String key = (String)keys.nextElement();
+            String value = (String)p.get(key);
+            map.put(key, value);
+        }
+
+        return map;
+        */
+        return System.getProperties();
+    }
+
 }
